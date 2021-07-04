4561f22d3b57cef5421e2c03a10365c11f1ba015
Ronald Rajski <ronaldrajski@gmail.com>
2021-07-03 23:08:56 -0500

    bankAccount updated

--- src/academy/learnProgramming/BankAccount/BankAccount.java
+++ src/academy/learnProgramming/BankAccount/BankAccount.java
@@ -16,4 +16,30 @@ public class BankAccount {
         this.balance = balance;
         this.accountType = typeOfAccount;
     }
+
+    // The branch argument is true if the customer is performing the transaction
+    // at a branch, with a teller.
+    // It's false if the customer is performing the transaction at an ATM
+    public double deposit(double amount, boolean branch){
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount, boolean branch){
+        if((amount>500.00) & !branch){
+            throw new IllegalArgumentException();
+        }
+
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
+    }
+
 }
